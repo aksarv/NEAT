d31506413cbc455039378b7c5ebf154fdf93fb58
aksarv <119102043+aksarv@users.noreply.github.com>
2025-07-25 12:28:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
 import copy
 
+# store history of innovation numbers of genes
 innovation_number = 0
 innovation_numbers = {}
 
+# define constants
 DISABLE_PROBABILITY = 0.5
 
 EXCESS_COEFFICIENT = 1
@@ -20,6 +22,7 @@ NUM_INPUTS = 5
 NUM_OUTPUTS = 2
 
 
+# get gene from innovation number
 def get_gene(innov):
     for gene in innovation_numbers:
         if innovation_numbers[gene] == innov:
@@ -33,6 +36,7 @@ class Gene:
         self.out = out
         self.weight = np.random.randn()
         self.enabled = True
+        # give next free if new input/output
         if (inp, out) in innovation_numbers:
             self.innov = innovation_numbers[(inp, out)]
         else:
@@ -40,6 +44,7 @@ class Gene:
             innovation_numbers[(inp, out)] = self.innov
         innovation_number += 1
 
+    # to get state of genome
     def __repr__(self):
         return f"Weight: {round(self.weight, 4)}\tEnabled: {self.enabled}\tInnovation Number: {self.innov}"
 
@@ -58,6 +63,7 @@ class Genome:
         self.bias = np.random.randn()
         for _ in range(o):
             self.add_node("o")
+        # starts fully connected from inputs to outputs
         if default_init:
             for n_id, n in self.nodes.items():
                 for n1_id, n1 in self.nodes.items():
@@ -71,6 +77,7 @@ class Genome:
         self.node_id += 1
         self.nodes[self.node_id] = t
 
+    # start at each output node and get the values of nodes before it recursively
     def feed_forward(self, input, node=None):
         if node is None:
             outputs = []
@@ -94,9 +101,11 @@ class Genome:
                         s += gene.weight * self.feed_forward(input, node=gene.inp)
                 return s
 
+    # is a certain input/output pair corresponding to a gene present in the genome
     def has_gene(self, gene_tuple):
         return gene_tuple in self.genes
 
+    # assumes self is the fitter parent - crosses over two parents to give a child genome
     def crossover(self, genome):
         child = Genome(self.i, self.o, default_init=False)
         max_innov = max(max([g.innov for g in self.genes.values()]), max([g.innov for g in genome.genes.values()]))
@@ -112,13 +121,16 @@ class Genome:
                 child.genes[gene_i] = self.genes[gene_i]
         return child
 
+    # after crossover gives a child from two parents a sequence of mutations will be applied randomly
     def mutate(self):
+        # either the weight is changed by a small gaussian or completely reassignmed
         if np.random.random() < PROB_MUTATE_WEIGHTS:
             random_gene = np.random.choice(self.genes.keys())
             self.genes[random_gene] += np.random.normal(loc=0, scale=0.1)
         else:
             random_gene = np.random.choice(self.genes.keys())
             self.genes[random_gene] = np.random.randn()
+        # connect two previously disconnected nodes. since this is feedforward neat we strictly need to connect in order of the forward pass
         if np.random.random() < PROB_ADD_CONNECTION:
             while True:
                 from_node = np.random.choice(self.nodes.keys())
@@ -130,6 +142,7 @@ class Genome:
                 if (from_node, to_node) not in self.genes:
                     break
             self.genes[(from_node, to_node)] = Gene(from_node, to_node)
+        # add a node, separating a connection into two connections, one to the intermediary node and one to the original final node
         if np.random.random() < PROB_ADD_NODE:
             connection = np.random.choice(self.genes.keys())
             from_node, to_node = connection
@@ -139,6 +152,7 @@ class Genome:
             self.genes[(from_node, new_node)] = Gene(from_node, new_node)
             self.genes[(new_node, to_node)] = Gene(from_node, new_node)
 
+    # needed for speciation - counts the number of disjoint and excess genes between two genomes
     def count_disjoint_excess_weight_diff(self, genome):
         max_innov = max(max([g.innov for g in self.genes.values()]), max([g.innov for g in genome.genes.values()]))
         disjoint, excess = 0, 0
@@ -171,6 +185,7 @@ class Genome:
             return 1
         return N
 
+    # get how compatible two genomes are as per the formula
     def compatibility(self, genome):
         disjoint, excess, weight_diff = self.count_disjoint_excess_weight_diff(genome)
         N = self.get_normalizing_factor(genome)
@@ -181,6 +196,7 @@ class Species:
     def __init__(self):
         self.members = []
 
+    # use the first genome in the species as the comparison to decide whether a new genome is to be added
     def add_genome(self, to_add):
         if not self.members:
             self.members.append(to_add)
@@ -190,6 +206,7 @@ class Species:
             return True
         return False
 
+    # select parents proportional to their fitness
     def select_parent(self):
         rand = np.random.random()
         total_fitness = sum(m.fitness for m in self.members)
@@ -202,6 +219,7 @@ class Species:
             if fitness_sums[i] < rand < fitness_sums[i + 1]:
                 return self.members[i]
 
+    # cull the bottom half in terms of fitness, then replace those lost with the fitter individuals' children
     def reproduce(self):
         self.members.sort(key=lambda l: l.fitness)
         prev_length = len(self.members)
@@ -221,6 +239,7 @@ class Population:
     def __init__(self):
         self.species = []
 
+    # add a genome to an existing species - if none are compatible, a new species is created
     def add_genome(self, genome):
         for species in self.species:
             if species.add_genome(genome):
@@ -230,6 +249,7 @@ class Population:
             new_species.add_genome(genome)
             self.species.append(new_species)
 
+    # call reproduce on each species
     def update_population(self):
         for species in self.species:
             species.reproduce()
